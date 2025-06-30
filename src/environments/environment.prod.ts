78757ddc1aba7ece474674353966298b02595890
Ioannis Koukotzilas <ioannis@monoscopic.net>
2025-06-30 08:32:04 +0300

    fix for environment variable in angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   siteUrl: 'https://greecepreciousmoments.com',
-  graphqlUrl: 'https://api.greecepreciousmoments.com/graphql',
+  graphqlUrl: 'https://api-gpm.monoscopic.net/graphql',
 };
